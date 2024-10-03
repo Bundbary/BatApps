9a97d166ebb32f167deff5d1d26a3d06e9211c74
Ferdinand Thubble <Bundbary@BradPennington.us>
2024-10-03 09:00:45 -0400

    slight improvement

--- UsedLessOften/GetVideoInfo.bat
+++ UsedLessOften/GetVideoInfo.bat
@@ -1,8 +1,15 @@
+
 @REM this file creates a json output of all of the file properties.
 set /p "confirm=Press Y if you ran this file on purpose. (Y/N): "
 if /i "%confirm%" neq "Y" goto :eof
 
 set /p "video_path=Enter the path to the video file: "
-@REM set "video_path=c:\Users\bpenn\ExpectancyLearning\Reckitt\Video\JeremyNarratedPrimary\mp4\DJI_20240724094314_0023_D.MP4"
-ffprobe -v quiet -print_format json -show_format -show_streams "%video_path%" > video_info.json
+
+@REM Extract the file name without extension
+for %%F in ("%video_path%") do set "file_name=%%~nF"
+
+@REM Create the output JSON file name
+set "output_file=%file_name%_video_info.json"
+
+ffprobe -v quiet -print_format json -show_format -show_streams "%video_path%" > "%output_file%"
 pause
